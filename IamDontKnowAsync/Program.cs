82c93382e3dec1d1ff56d9ce2b2425db9c8ac103
Alexander Puzynia <werwolf.by@gmail.com>
2017-03-21 02:46:18 +0300

    feat(sync): simple sync request method

---
+++ IamDontKnowAsync/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IamDontKnowAsync
+{
+    class Program
+    {
+        static void Main()
+        {
+            MakeRequest();
+        }
+
+        public static void MakeRequest()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            const string host = "localhost";
+
+            socket.Connect(new DnsEndPoint(host, 80));
+
+            var request = $"GET http://{host}/ HTTP/1.1\r\n" +
+                          "Host: localhost\r\n" +
+                          "Accept: text/html\r\n\r\n";
+
+            var buffer = Encoding.ASCII.GetBytes(request);
+            var sended = socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+            Console.WriteLine($"Request sended: {sended}");
+
+            socket.Disconnect(false);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Everything .NET programmers know about Asynchronous Programming is wrong
+
+Live Demo application
