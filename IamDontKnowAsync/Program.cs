8d30130929c266adf0bba6d60ccdb120931373b9
Alexander Puzynia <werwolf.by@gmail.com>
2017-03-21 03:00:29 +0300

    feat(async): call Connect asynchronously

--- IamDontKnowAsync/Program.cs
+++ IamDontKnowAsync/Program.cs
@@ -19,34 +19,37 @@ namespace IamDontKnowAsync
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
 
             const string host = "localhost";
-
-            socket.Connect(new DnsEndPoint(host, 80));
-
-            var request = $"GET http://{host}/ HTTP/1.1\r\n" +
-                          "Host: localhost\r\n" +
-                          "Accept: text/html\r\n\r\n";
-
-            var buffer = Encoding.ASCII.GetBytes(request);
-            socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, asyncResult1 =>
+            
+            socket.BeginConnect(new DnsEndPoint(host, 80), asyncResult0 =>
             {
-                var sended = socket.EndSend(asyncResult1);
+                socket.EndConnect(asyncResult0);
 
-                Console.WriteLine($"Request sended: {sended}");
+                var request = $"GET http://{host}/ HTTP/1.1\r\n" +
+                              "Host: localhost\r\n" +
+                              "Accept: text/html\r\n\r\n";
 
-                var response = new byte[1 * 1024 * 1024];
-
-                socket.BeginReceive(response, 0, response.Length, SocketFlags.None, asyncResult2 =>
+                var buffer = Encoding.ASCII.GetBytes(request);
+                socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, asyncResult1 =>
                 {
-                    var received = socket.EndReceive(asyncResult2);
+                    var sended = socket.EndSend(asyncResult1);
 
-                    Console.WriteLine($"Response received: {received}");
-                    Console.WriteLine(Encoding.UTF8.GetString(response, 0, received));
+                    Console.WriteLine($"Request sended: {sended}");
 
-                    socket.BeginDisconnect(false, asyncResult3 =>
+                    var response = new byte[1 * 1024 * 1024];
+
+                    socket.BeginReceive(response, 0, response.Length, SocketFlags.None, asyncResult2 =>
                     {
-                        socket.EndDisconnect(asyncResult3);
+                        var received = socket.EndReceive(asyncResult2);
 
-                        Console.WriteLine("Disconnected");
+                        Console.WriteLine($"Response received: {received}");
+                        Console.WriteLine(Encoding.UTF8.GetString(response, 0, received));
+
+                        socket.BeginDisconnect(false, asyncResult3 =>
+                        {
+                            socket.EndDisconnect(asyncResult3);
+
+                            Console.WriteLine("Disconnected");                        
+                        }, null);
                     }, null);
                 }, null);
             }, null);
