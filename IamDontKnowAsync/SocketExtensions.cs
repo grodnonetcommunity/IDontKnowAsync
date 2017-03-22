cdff0f8f587ca28c8865373a2dcd9fccf5627db7
Alexander Puzynia <werwolf.by@gmail.com>
2017-03-22 11:20:34 +0300

    feat(async4): use AttachedToParent option for child tasks

--- IamDontKnowAsync/SocketExtensions.cs
+++ IamDontKnowAsync/SocketExtensions.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IamDontKnowAsync
 {
     public static class SocketExtensions
     {
-        public static Task ConnectTask(this Socket socket, EndPoint remote)
+        public static Task ConnectTask(this Socket socket, EndPoint remote, TaskCreationOptions creationOptions = TaskCreationOptions.None)
         {
-            return Task.Factory.FromAsync(socket.BeginConnect, socket.EndConnect, remote, null);
+            return Task.Factory.FromAsync(socket.BeginConnect, socket.EndConnect, remote, null, creationOptions);
         }
 
-        public static Task<int> SendTask(this Socket socket, byte[] buffer, int offset, int count)
+        public static Task<int> SendTask(this Socket socket, byte[] buffer, int offset, int count, TaskCreationOptions creationOptions = TaskCreationOptions.None)
         {
-            return Task.Factory.FromAsync(socket.BeginSend, socket.EndSend, buffer, offset, count, null);
+            return Task.Factory.FromAsync(socket.BeginSend, socket.EndSend, buffer, offset, count, null, creationOptions);
         }
 
-        public static Task<int> ReceivedTask(this Socket socket, byte[] buffer, int offset, int count)
+        public static Task<int> ReceivedTask(this Socket socket, byte[] buffer, int offset, int count, TaskCreationOptions creationOptions = TaskCreationOptions.None)
         {
-            return Task.Factory.FromAsync(socket.BeginReceive, socket.EndReceive, buffer, offset, count, null);
+            return Task.Factory.FromAsync(socket.BeginReceive, socket.EndReceive, buffer, offset, count, null, creationOptions);
         }
 
-        public static Task DisconnectTask(this Socket socket, bool reuseSocket)
+        public static Task DisconnectTask(this Socket socket, bool reuseSocket, TaskCreationOptions creationOptions = TaskCreationOptions.None)
         {
-            return Task.Factory.FromAsync(socket.BeginDisconnect, socket.EndDisconnect, reuseSocket, null);
+            return Task.Factory.FromAsync(socket.BeginDisconnect, socket.EndDisconnect, reuseSocket, null, creationOptions);
         }
 
         private static IAsyncResult BeginSend(this Socket socket, byte[] buffer, int offset, int count,
