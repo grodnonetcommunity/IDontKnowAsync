e3b45e54b9445de4272a5ac578260ca17e44203a
Alexander Puzynia <werwolf.by@gmail.com>
2017-03-24 15:12:59 +0300

    feat(wpf): add WpfAsync project

---
+++ WpfAsync/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfAsync.Annotations;
+
+namespace WpfAsync
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _result;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (value == _result) return;
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfAsync/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfAsync
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = ViewModel = new MainViewModel();
+        }
+
+        public MainViewModel ViewModel { get; }
+
+        private void MakeItBeautifulOnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Result = "Asyn is Beautiful";
+        }
+    }
+}
