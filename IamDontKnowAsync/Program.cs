ff38a00f864991fdfe290809b0130b5b5ea29aa7
Alexander Puzynia <werwolf.by@gmail.com>
2017-04-19 01:29:04 +0300

    feat(Redundancy): throw exception from google server

--- IamDontKnowAsync/Program.cs
+++ IamDontKnowAsync/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IamDontKnowAsync
@@ -13,12 +14,18 @@ namespace IamDontKnowAsync
 
         static void Main()
         {
-            MakeRequest();
+            MakeRequest().Wait();
+
+            GC.Collect(2);
+            GC.WaitForPendingFinalizers();
+
+            GC.Collect(2);
+            GC.WaitForPendingFinalizers();
 
             Console.ReadLine();
         }
 
-        public static async void MakeRequest()
+        public static async Task MakeRequest()
         {
             var s1 = GetBuyRecomendation("http://google.com");
             var s2 = GetBuyRecomendation("http://microsoft.com");
@@ -34,6 +41,12 @@ namespace IamDontKnowAsync
         public static async Task<(string server, bool recomended)> GetBuyRecomendation(string server)
         {
             await Task.Delay(Rnd.Next(100, 1000));
+
+            if (server == "http://google.com")
+            {
+                Console.WriteLine("Damn...");
+                throw new Exception("Web exception");
+            }
 
             return (server: server, recomended: true);
         }
