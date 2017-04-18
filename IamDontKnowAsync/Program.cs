0b619e57d1572b175a7d9b3077d93c8dc7efba9b
Alexander Puzynia <werwolf.by@gmail.com>
2017-04-19 00:23:03 +0300

    feat(core): clear MakeRequest

--- IamDontKnowAsync/Program.cs
+++ IamDontKnowAsync/Program.cs
@@ -16,30 +16,6 @@ namespace IamDontKnowAsync
 
         public static async void MakeRequest()
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-
-            const string host = "localhost";
-
-            await socket.ConnectTask(new DnsEndPoint(host, 80));
-            var request = $"GET http://{host}/ HTTP/1.1\r\n" +
-                          "Host: localhost\r\n" +
-                          "Accept: text/html\r\n\r\n";
-
-            var buffer = Encoding.ASCII.GetBytes(request);
-            var sended = await socket.SendTask(buffer, 0, buffer.Length);
-
-            Console.WriteLine($"Request sended: {sended}");
-
-            var response = new byte[1 * 1024 * 1024];
-
-            var received = await socket.ReceivedTask(response, 0, response.Length);
-
-            Console.WriteLine($"Response received: {received}");
-            Console.WriteLine(Encoding.UTF8.GetString(response, 0, received));
-
-            await socket.DisconnectTask(false);
-
-            Console.WriteLine("Disconnected");
         }
     }
 }
